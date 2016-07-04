d1fda8a69b39c5b2b61af4807c2400820fe32ad3
John Ryan <john.ryan@cardinalhealth.com>
2016-07-04 15:35:43 -0400

    restructuring

--- app/components/atm-display/atm-display.component.js
+++ app/components/atm-display/atm-display.component.js
@@ -7,7 +7,7 @@ component('atmDisplay', {
     controller: function AtmDisplayController($http) {
         var self = this;
 
-        $http.get('displays/displays.json').then(function(response) {
+        $http.get('assets/displays.json').then(function(response) {
             self.availableDisplays = response.data;
             self.currentDisplay = response.data["welcome"];
         });
