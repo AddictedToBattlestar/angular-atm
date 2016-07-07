a71ec39601eb0516e310e65c01024e0bb8d594b6
John Ryan <john.ryan@cardinalhealth.com>
2016-07-06 23:48:27 -0400

    PIN entry

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -13,17 +13,46 @@ angular.module('myApp.main', ['ngRoute', 'draganddrop'])
         var ctrl = this;
         ctrl.customerAtmCard = {
             'cardType': 'atmCard',
-            'cardNumber': 123457898765432
+            'cardNumber': 123457898765432,
+            'PIN': 1234
         };
         ctrl.showCustomerAtmCard = true;
         ctrl.currentDisplay = '';
+        ctrl.keysPressed = 0;
+        ctrl.pinEntered = 0;
+        ctrl.cardInserted = null;
         
-        ctrl.onDrop = function (data, event) {
+        ctrl.processCardInserted = function (data, event) {
             console.log('drop works');
             var insertedCard = data['json/custom-object'];
             if (!insertedCard || insertedCard.cardType !== 'atmCard') return;
             console.log('An ATM card was found to be inserted')
             ctrl.currentDisplay = 'enterPinNumber';
             ctrl.showCustomerAtmCard = false;
+            ctrl.cardInserted = ctrl.customerAtmCard;
         };
+
+        ctrl.processNumberPadKeyPress = function (keyPressed) {
+            console.log('A key press of ' + keyPressed + ' was registered');
+            if(!ctrl.cardInserted) return;
+            switch (ctrl.keysPressed) {
+                case 0:
+                    ctrl.pinEntered = keyPressed;
+                    ctrl.currentDisplay = 'enterPinNumber-1Character';
+                    break;
+                case 1:
+                    ctrl.pinEntered = ctrl.pinEntered + (10 * keyPressed);
+                    ctrl.currentDisplay = 'enterPinNumber-2Characters';
+                    break;
+                case 2:
+                    ctrl.pinEntered = ctrl.pinEntered + (100 * keyPressed);
+                    ctrl.currentDisplay = 'enterPinNumber-3Characters';
+                    break;
+                case 3:
+                    ctrl.pinEntered = ctrl.pinEntered + (1000 * keyPressed);
+                    ctrl.currentDisplay = 'enterPinNumber-Complete';
+                    break;
+            }
+            ctrl.keysPressed++;
+        }
     }]);
