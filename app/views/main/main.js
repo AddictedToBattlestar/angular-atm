1deb10cc7faeaf65d9e5d74082992fd42cd8faf6
John Ryan <john.ryan@cardinalhealth.com>
2016-07-07 21:46:58 -0400

    I am not feeling good on the direction with this

--- app/views/main/main.js
+++ app/views/main/main.js
@@ -11,18 +11,15 @@ angular.module('myApp.main', ['ngRoute', 'draganddrop'])
 
     .controller('MainCtrl', [function () {
         var ctrl = this;
+        
         ctrl.customerAtmCard = {
             'cardType': 'atmCard',
             'cardNumber': 123457898765432,
-            'PIN': 1234
+            'PIN': '1234'
         };
-        ctrl.showCustomerAtmCard = true;
-        ctrl.currentDisplay = '';
-        ctrl.keysPressed = 0;
-        ctrl.pinEntered = 0;
+        ctrl.showCustomerAtmCard = true;        
         ctrl.cardInserted = null;
-        
-        ctrl.processCardInserted = function (data, event) {
+        ctrl.cardInserted = function (data, event) {
             console.log('drop works');
             var insertedCard = data['json/custom-object'];
             if (!insertedCard || insertedCard.cardType !== 'atmCard') return;
@@ -32,28 +29,48 @@ angular.module('myApp.main', ['ngRoute', 'draganddrop'])
             ctrl.cardInserted = ctrl.customerAtmCard;
         };
 
-        ctrl.processNumberPadKeyPress = function (keyPressed) {
+        ctrl.keysPressed = 0;
+        ctrl.pinEntered = 0;
+        ctrl.correctPinEntered = false;
+        ctrl.numberPadKeyClick = function (keyPressed) {
             console.log('A key press of ' + keyPressed + ' was registered');
             if(!ctrl.cardInserted) return;
+            if (keyPressed === 'ENTER') {
+                ctrl.enterButtonClick();
+            } else if (keyPressed === 'CLEAR') {
+                ctrl.pinEntered = '';
+                ctrl.keysPressed = 0;
+                ctrl.currentDisplay = 'enterPinNumber';
+            } else {
+                ctrl.processNumberKey(keyPressed);
+            }
+        }
+
+        ctrl.processNumberKey = function(keyPressed) {
             switch (ctrl.keysPressed) {
                 case 0:
                     ctrl.pinEntered = keyPressed;
                     ctrl.currentDisplay = 'enterPinNumber-1Character';
                     break;
                 case 1:
-                    ctrl.pinEntered = ctrl.pinEntered + (10 * keyPressed);
+                    ctrl.pinEntered = ctrl.pinEntered + keyPressed;
                     ctrl.currentDisplay = 'enterPinNumber-2Characters';
                     break;
                 case 2:
-                    ctrl.pinEntered = ctrl.pinEntered + (100 * keyPressed);
+                    ctrl.pinEntered = ctrl.pinEntered + keyPressed;
                     ctrl.currentDisplay = 'enterPinNumber-3Characters';
                     break;
                 case 3:
-                    ctrl.pinEntered = ctrl.pinEntered + (1000 * keyPressed);
+                    ctrl.pinEntered = ctrl.pinEntered + keyPressed;
                     ctrl.currentDisplay = 'enterPinNumber-Complete';
+                    ctrl.correctPinEntered = (ctrl.pinEntered === ctrl.cardInserted.PIN);
                     break;
             }
             console.log('PIN entered at present: ' + ctrl.pinEntered);
             ctrl.keysPressed++;
+        }
+
+        ctrl.enterButtonClick = function() {
+            if (ctrl.correctPinEntered) ctrl.currentDisplay = 'selectTransaction';
         }
     }]);
