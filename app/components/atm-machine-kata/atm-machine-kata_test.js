352468914645f752bbe67246e51f6464e0262620
John Ryan <john.ryan@cardinalhealth.com>
2016-07-08 14:03:05 -0400

    alright, I am going to build the atm machine logic through tests here and then wire it into the app.

---
+++ app/components/atm-machine-kata/atm-machine-kata_test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('atm-machine-kata', function() {
+    describe('when started', function() {
+        it('error test', function() {
+            expect(2+2).toEqual(3);
+        });
+    });
+});
