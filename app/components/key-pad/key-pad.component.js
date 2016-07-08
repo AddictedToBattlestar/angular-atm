2ef8c4067fc23663b5ae8dbeea4598d4679b31e9
John Ryan <john.ryan@cardinalhealth.com>
2016-07-07 20:47:37 -0400

    refactor

--- app/components/key-pad/key-pad.component.js
+++ app/components/key-pad/key-pad.component.js
@@ -5,10 +5,6 @@ module('myApp.keyPad').
 component('keyPad', {
     templateUrl: 'components/key-pad/key-pad.template.html',
     controller: function() {
-        var ctrl = this;
-        ctrl.keyPress = function(keyPressed) {
-            ctrl.processNumberPadKeyPress({'keyPressed': keyPressed});
-        }
     },
     bindings: {
         processNumberPadKeyPress: '&'
